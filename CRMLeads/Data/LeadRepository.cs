b57bd2624ecbe4333c539c26480e8b5f3233e9d4
muees22 <mughees.islam@yahoo.com>
2023-10-12 20:56:20 +0500

    New Model Class and DataRepository class Added

---
+++ CRMLeads/Data/LeadRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace CRMLeads.Data
+
+{
+    public class LeadRepository
+    {
+        private SqlConnection _connection;
+
+        public LeadRepository()
+        {
+            //string connStr = "Server ..... Database ...... ; TrustedConnection= True; ......"
+        }
+    }
+}
